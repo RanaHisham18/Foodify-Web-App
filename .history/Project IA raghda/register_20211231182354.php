01985a657fceff5b55779793711a2f8f66ac72be
Rana Hisham <hishamrana00@gmail.com>
2022-01-05 21:12:48 +0200

    nav bar changed to be respnsive in all pages

--- .history/Project IA raghda/register_20211231182354.php
+++ .history/Project IA raghda/register_20211231182354.php
@@ -92,6 +92,6 @@
         <p>Already have an account? <a href="#">Sign in</a>.</p>
       </div>
     </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>  
+    
 </body>
 </html>
