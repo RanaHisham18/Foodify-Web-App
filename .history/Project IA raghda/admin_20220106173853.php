fdb9f4d25d3a438564f38687386992fb5d366797
Rana Hisham <hishamrana00@gmail.com>
2022-01-06 17:45:26 +0200

    modifications in navbar(reciepes removed other than in home page)

--- .history/Project IA raghda/admin_20220106173853.php
+++ .history/Project IA raghda/admin_20220106173853.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-     <link rel="stylesheet" href="records.css" type="text/css">
-    <title>displaying records</title>
+     <link  type="text/css" rel="stylesheet" href="admin.css">
+    <title>Admin Search</title>
 </head>
 <body>
-
+    
  <!--Navigation Bar-->
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
      <img class="image" src="images/foodify.png" alt="Foodify logo"> 
@@ -30,52 +31,83 @@
               <li class="nav-item">
                 <a class="nav-link" href="login.php">Log in</a>
               </li>
-           
+              <li class="nav-item">
+                <a class="nav-link" href="#recipes">Recipes</a>
+
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.php">Log out</a>
+
+              </li>
             </ul>
             
           </div>
         </div>
     </nav>
-<h2>Recipes Details for Admin</h2>
-<center>
+
+    
+    <header><h3>Admin interface</h3></header>
+    <form method="POST">
+        <label class="label">look for something</label>
+        <input type="text" name="search" placeholder="search by ID" autocomplete="off" required>  <br> <br>
+        <input type="submit" name="submit" value="search !">
+    </form>
+    <h6> To display all reciepes from database click here </h6>
+ <a href="records.php" > See more</a>
+<?php 
+
+include 'server1.php';
+$con = 'server1.php';
+if(isset($_POST['submit'])){
+    $ss = $_POST['search'];
+    $query= "SELECT * FROM `reciepes` WHERE `id`='$ss'";
+    $ress = mysqli_query($db,$query);
+
+if ($roww = mysqli_fetch_array($ress)){
+
+?>
+    <br/><br/>
+    <center> 
     <table border="2">
     <tr class="table">
-      
         <th>Name</th>
         <th>ID</th>
-        <th>Category</th>
-        <th>Image</th>
-        <th>edit</th>
-        <th>delete</th>
-    </tr>
+        <th>color</th>
+        <th>Edit</th>
+        <th>Delete</th>
 
-    <?php
 
-    include "server1.php"; //database connection
-    $queryselect = 'select*from reciepes';
-    $records=mysqli_query($db,$queryselect); //fetch data
-    //$data = $records;
+    </tr>           
 
-    while ($data = mysqli_fetch_array($records))
-{
-    ?>
     <tr>
-        <Td class="text"><?php echo $data['name'] ?> </Td>
-        <Td class="text"><?php echo $data['id'] ?> </Td>
-        <Td class="text"><?php echo $data['category'] ?> </Td>
-        <td><img src=" <?php echo $data['image']; ?>" width = "200px", height="200px"/> </td>
-        <td><a href=" edit.php?id= <?php echo $data['id']; ?>">Edit</a></td>
-        <td><a href=" delete.php?id= <?php echo $data['id']; ?>">Delete</a></td>
+        <Td><?php echo $roww['name'] ?> </Td>
+        <Td><?php echo $roww['id'] ?> </Td>
+        <Td><?php echo $roww['category'] ?> </Td>
+        <td><a href=" edit.php?id= <?php echo $roww['id']; ?>">Edit</a></td>
+        <td><a href=" delete.php?id= <?php echo $roww['id']; ?>">Delete</a></td>
+
+
+
 
     </tr>
+    </table>
+    </center>
 
     <?php
-          }
-    ?>
-</center>
-</table>
+}
+else{
+    echo "Name not found";
 
 
+} }
+
+
+
+?>
+
+
+
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
